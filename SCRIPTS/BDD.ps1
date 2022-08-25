ed5e3c4bc0610c1a0608da5cd158814ffd12cd9b
Adri-Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2022-08-25 16:22:49 +0200

    Correction du script

--- SCRIPTS/BDD.ps1
+++ SCRIPTS/BDD.ps1
@@ -1,52 +1,56 @@
 $cigales_codes = Import-Csv "CIGALES-CODES.csv"
+Remove-Item "./INATURALIST/*.csv"
 $cigales_codes | ForEach-Object {
-		$nom = $_.NOM_SCIENTIFIQUE
-		$onem = $_.ONEM
-		$faune_france = $_.FAUNEFRANCE
-		$inpn = $_.INPN
-		$inaturalist = $_.INATURALIST
-		$observation = $_.OBSERVATION
-		
-		#ONEM - voir https://github.com/cigalesdefrance/cigalesdefrance.github.io/blob/main/SCRIPTS/sauvegarde_onem.ps1
-		
-		#FAUNE-FRANCE
-		"Faune-France - $nom"
-		if ($faune_france -eq "") {
-		"Pas de données pour l'espèce dans Faune-France" }
-		else {
-		Invoke-WebRequest -Uri "https://www.faune-france.org/index.php?m_id=95&sp_tg=19&sp_DChoice=all&sp_SChoice=species&sp_PChoice=all&sp_FChoice=map&sp_S=$faune_france" -OutFile "./FAUNE-FRANCE/$nom.png" }
-		
-		#INPN
-		
-		#INATURALIST
-		"Inaturalist - $nom"
-		if ($inaturalist -eq "") {
-		"Pas de données pour l'espèce dans Inaturalist" }
-		else {
+	$nom = $_.NOM_SCIENTIFIQUE
+	$onem = $_.ONEM
+	$faune_france = $_.FAUNEFRANCE
+	$inpn = $_.INPN
+	$inaturalist = $_.INATURALIST
+	$observation = $_.OBSERVATION
+	
+	#ONEM - voir https://github.com/cigalesdefrance/cigalesdefrance.github.io/blob/main/SCRIPTS/sauvegarde_onem.ps1
+	
+	#FAUNE-FRANCE
+	"Faune-France - $nom"
+	if ($faune_france -eq "") {
+	"  > Pas de données pour l'espèce dans Faune-France" }
+	else {
+	Invoke-WebRequest -Uri "https://www.faune-france.org/index.php?m_id=95&sp_tg=19&sp_DChoice=all&sp_SChoice=species&sp_PChoice=all&sp_FChoice=map&sp_S=$faune_france" -OutFile "./FAUNE-FRANCE/$nom.png" }
+	
+	#INPN
+	
+	#INATURALIST
+	"Inaturalist - $nom"
+	if ($inaturalist -eq "") {
+	"  > Pas de données pour l'espèce dans Inaturalist" }
+	else {
 		Add-Content "./INATURALIST/$nom.csv" "Latitude,Longitude"
 		$total_results = (Invoke-WebRequest "https://api.inaturalist.org/v1/observations?&place_id=6753&taxon_id=$inaturalist" | ConvertFrom-Json).total_results
 		$pages = [math]::ceiling($total_results/200)
 		for ($num=1;$num -le $pages;$num++) {
-		"page $num sur $pages"
+			"page $num sur $pages"
 		(Invoke-WebRequest "https://api.inaturalist.org/v1/observations?&place_id=6753&taxon_id=$inaturalist&page=$num&per_page=200" | ConvertFrom-Json).results.location | Add-Content "./INATURALIST/$nom.csv" }		
-		}
-		
-		#OBSERVATION.ORG
-		"Observation.org - $nom"
-		if ($observation -eq "") {
-		"Pas de données pour l'espèce dans Observation" }
-		else {
+	}
+	
+	#OBSERVATION.ORG
+	"Observation.org - $nom"
+	if ($observation -eq "") {
+	"  > Pas de données pour l'espèce dans Observation" }
+	else {
 		[xml]$data = (invoke-webrequest -Uri "https://france.observation.org/kmlloc/soort_get_xml_points.php?soort=$observation")
 		$observation = $data.markers.line.point
-		$observation | Out-File "observation.txt"
-		(Get-Content "observation.txt" | Select-Object -Skip 2) | Set-Content "observation.txt"
-		$observation = Get-Content "observation.txt"
-		$observation[0] = "Latitude,Longitude"
-		$observation = $observation -replace " ",","
-		$observation | Out-File "./OBSERVATION/$nom.csv"
-		Remove-Item "observation.txt" }
-    }
-
+		if ($observation -eq "") {
+			"  > Pas de données pour l'espèce dans Observation"
+			else {
+				$observation | Out-File "observation.txt"
+				(Get-Content "observation.txt" | Select-Object -Skip 2) | Set-Content "observation.txt"
+				$observation = Get-Content "observation.txt"
+				$observation[0] = "Latitude,Longitude"
+				$observation = $observation -replace " ",","
+				$observation | Out-File "./OBSERVATION/$nom.csv"
+			Remove-Item "observation.txt" }}
+	}
+}
 # git
 git config --local user.email "cigalesdefrance@outlook.fr"
 git config --local user.name "cigalesdefrance"
