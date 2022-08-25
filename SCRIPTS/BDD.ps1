2101ca36677ebf3e0f1f1b07bae9f30fb81ebc1f
Adri-Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2022-08-25 15:17:08 +0200

    Ajout des téléchargements des BDD

---
+++ SCRIPTS/BDD.ps1
@@ -0,0 +1,45 @@
+$cigales_codes = Import-Csv "CIGALES-CODES.csv"
+$cigales_codes | ForEach-Object {
+		$nom = $_.NOM_SCIENTIFIQUE
+		$onem = $_.ONEM
+		$faune_france = $_.FAUNEFRANCE
+		$inpn = $_.INPN
+		$inaturalist = $_.INATURALIST
+		$observation = $_.OBSERVATION
+		
+		#ONEM - voir https://github.com/cigalesdefrance/cigalesdefrance.github.io/blob/main/SCRIPTS/sauvegarde_onem.ps1
+		
+		#FAUNE-FRANCE
+		Invoke-WebRequest -Uri "https://www.faune-france.org/index.php?m_id=95&sp_tg=19&sp_DChoice=all&sp_SChoice=species&sp_PChoice=all&sp_FChoice=map&sp_S=$faune_france" -OutFile "./FAUNE-FRANCE/$nom.png"
+		
+		#INPN
+		
+		#INATURALIST
+		Add-Content "./INATURALIST/$nom.csv" "Latitude,Longitude"
+		$total_results = (Invoke-WebRequest "https://api.inaturalist.org/v1/observations?&place_id=6753&taxon_id=$inaturalist" | ConvertFrom-Json).total_results
+		$pages = [math]::ceiling($total_results/200)
+		for ($num=1;$num -le $pages;$num++) {
+		"page $num"
+		(Invoke-WebRequest "https://api.inaturalist.org/v1/observations?&place_id=6753&taxon_id=$inaturalist&page=$num&per_page=200" | ConvertFrom-Json).results.location | Add-Content "./INATURALIST/$nom.csv"
+		
+		}
+		
+		#OBSERVATION.ORG
+		[xml]$data = (invoke-webrequest -Uri "https://france.observation.org/kmlloc/soort_get_xml_points.php?soort=$observation")
+		$observation = $data.markers.line.point
+		$observation | Out-File "observation.txt"
+		(Get-Content "observation.txt" | Select-Object -Skip 2) | Set-Content "observation.txt"
+		$observation = Get-Content "observation.txt"
+		$observation[0] = "Latitude,Longitude"
+		$observation = $observation -replace " ",","
+		$observation | Out-File "./OBSERVATION/$nom.csv"
+		Remove-Item "observation.txt"
+    }
+}
+
+# git
+git config --local user.email "cigalesdefrance@outlook.fr"
+git config --local user.name "cigalesdefrance"
+git add .
+git commit -m "[Bot] Téléchargement des bases de données"
+git push -f
