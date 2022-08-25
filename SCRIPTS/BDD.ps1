6eb4e45f535ff196f2362ac0145a59273b76619b
Adri-Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2022-08-25 15:43:08 +0200

    Correction du script des téléchargements

--- SCRIPTS/BDD.ps1
+++ SCRIPTS/BDD.ps1
@@ -10,21 +10,32 @@ $cigales_codes | ForEach-Object {
 		#ONEM - voir https://github.com/cigalesdefrance/cigalesdefrance.github.io/blob/main/SCRIPTS/sauvegarde_onem.ps1
 		
 		#FAUNE-FRANCE
-		Invoke-WebRequest -Uri "https://www.faune-france.org/index.php?m_id=95&sp_tg=19&sp_DChoice=all&sp_SChoice=species&sp_PChoice=all&sp_FChoice=map&sp_S=$faune_france" -OutFile "./FAUNE-FRANCE/$nom.png"
+		"Faune-France - $nom"
+		if ($faune_france -eq "") {
+		"Pas de données pour l'espèce dans Faune-France" }
+		else {
+		Invoke-WebRequest -Uri "https://www.faune-france.org/index.php?m_id=95&sp_tg=19&sp_DChoice=all&sp_SChoice=species&sp_PChoice=all&sp_FChoice=map&sp_S=$faune_france" -OutFile "./FAUNE-FRANCE/$nom.png" }
 		
 		#INPN
 		
 		#INATURALIST
+		"Inaturalist - $nom"
+		if ($faune_france -eq "") {
+		"Pas de données pour l'espèce dans INaturalist" }
+		else {
 		Add-Content "./INATURALIST/$nom.csv" "Latitude,Longitude"
 		$total_results = (Invoke-WebRequest "https://api.inaturalist.org/v1/observations?&place_id=6753&taxon_id=$inaturalist" | ConvertFrom-Json).total_results
 		$pages = [math]::ceiling($total_results/200)
 		for ($num=1;$num -le $pages;$num++) {
-		"page $num"
-		(Invoke-WebRequest "https://api.inaturalist.org/v1/observations?&place_id=6753&taxon_id=$inaturalist&page=$num&per_page=200" | ConvertFrom-Json).results.location | Add-Content "./INATURALIST/$nom.csv"
-		
+		"page $num sur $pages"
+		(Invoke-WebRequest "https://api.inaturalist.org/v1/observations?&place_id=6753&taxon_id=$inaturalist&page=$num&per_page=200" | ConvertFrom-Json).results.location | Add-Content "./INATURALIST/$nom.csv" }		
 		}
 		
 		#OBSERVATION.ORG
+		"Observation.org - $nom"
+		if ($faune_france -eq "") {
+		"Pas de données pour l'espèce dans Observation.org" }
+		else {
 		[xml]$data = (invoke-webrequest -Uri "https://france.observation.org/kmlloc/soort_get_xml_points.php?soort=$observation")
 		$observation = $data.markers.line.point
 		$observation | Out-File "observation.txt"
@@ -33,12 +44,12 @@ $cigales_codes | ForEach-Object {
 		$observation[0] = "Latitude,Longitude"
 		$observation = $observation -replace " ",","
 		$observation | Out-File "./OBSERVATION/$nom.csv"
-		Remove-Item "observation.txt"
+		Remove-Item "observation.txt" }
     }
 
 # git
 git config --local user.email "cigalesdefrance@outlook.fr"
 git config --local user.name "cigalesdefrance"
 git add .
-git commit -m "[Bot] Téléchargement des bases de données"
+git commit -m "[Bot] Téléchargement des données"
 git push -f
