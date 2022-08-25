34dc374d2d4f1dda6f0c5f718024e56ce3c0f8af
Adri-Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2022-08-25 09:59:56 +0200

    Ajout de la sauvegarde des cartes de l'ONEM

---
+++ SCRIPTS/sauvegarde_onem.ps1
@@ -0,0 +1,24 @@
+$regex = '<a href="([^"]*)">(.[a-z].+)</a>.([a-z].+)<br>'
+
+foreach($line in Get-Content ./ONEM/index.html) {
+    if($line -match $regex){
+		$url = $matches[1]
+		$sci = $matches[2]
+		$sci = $sci -replace " ","_"
+		$ver = $matches[3]
+		Invoke-WebRequest -Uri $url -OutFile "sp.html"
+		$Source = Get-Content -path "sp.html" -raw
+		$Source -match 'tools/cartowiki/CACHE/(.*).jpg'
+		$Sourceurl = $matches[1]
+		Invoke-WebRequest -Uri "http://www.onem-france.org/cigales/tools/cartowiki/CACHE/$Sourceurl.jpg" -OutFile "./ONEM/$sci.jpg"
+
+    }
+}
+
+Remove-Item "sp.html"
+
+# git
+git config --local user.email "cigalesdefrance@outlook.fr"
+git config --local user.name "cigalesdefrance"
+git add .
+git commit -m "[Bot] Sauvegarde des cartes de l'ONEM" #--allow-empty
