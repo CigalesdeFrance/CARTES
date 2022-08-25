7bb121cae54c196699094ccf3c9b2629ee35ebbd
Adri-Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2022-08-25 10:01:48 +0200

    Correction et optimisation

--- SCRIPTS/sauvegarde_onem.ps1
+++ SCRIPTS/sauvegarde_onem.ps1
@@ -3,13 +3,14 @@ $regex = '<a href="([^"]*)">(.[a-z].+)</a>.([a-z].+)<br>'
 foreach($line in Get-Content ./ONEM/index.html) {
     if($line -match $regex){
 		$url = $matches[1]
-		$sci = $matches[2]
-		$sci = $sci -replace " ","_"
+		$sci1 = $matches[2]
+		$sci = $sci1 -replace " ","_"
 		$ver = $matches[3]
 		Invoke-WebRequest -Uri $url -OutFile "sp.html"
 		$Source = Get-Content -path "sp.html" -raw
 		$Source -match 'tools/cartowiki/CACHE/(.*).jpg'
 		$Sourceurl = $matches[1]
+        Echo $sci1
 		Invoke-WebRequest -Uri "http://www.onem-france.org/cigales/tools/cartowiki/CACHE/$Sourceurl.jpg" -OutFile "./ONEM/$sci.jpg"
 
     }
