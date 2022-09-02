1d426c93413a91c10f7c98b067d4904f1f014891
Adri-Charbonneau <105881448+Adri-Charbonneau@users.noreply.github.com>
2022-09-02 09:24:04 +0200

    Update BDD.ps1

--- SCRIPTS/BDD.ps1
+++ SCRIPTS/BDD.ps1
@@ -53,9 +53,50 @@ $cigales_codes | ForEach-Object {
 				$observation | Out-File "./BDD/OBSERVATION/$nom.csv"
 			Remove-Item "observation.txt" }}
 }
+
+### CREATION DES KML
+# OBSERVATION
+$files = Get-ChildItem "./BDD/OBSERVATION/" -Filter *.csv
+foreach ($f in $files){
+	$fichier = $f.Name
+    $espece = $f.Name -replace ".csv"
+	$kml =
+	"<?xml version=`"1.0`" encoding=`"UTF-8`"?><kml xmlns=`"http://www.opengis.net/kml/2.2`" xmlns:gx=`"http://www.google.com/kml/ext/2.2`" xmlns:kml=`"http://www.opengis.net/kml/2.2`" xmlns:atom=`"http://www.w3.org/2005/Atom`">
+	<Document>
+	<name>$espece</name>
+	<Folder>
+	<name>INATURALIST</name>
+	$(Import-Csv "./BDD/OBSERVATION/$fichier" | foreach {'<Placemark><IconStyle><color>red</color><Icon></Icon></IconStyle><Point><coordinates>{1},{0}</coordinates></Point></Placemark>' -f $_.Latitude, $_.Longitude})
+	</Folder>
+	</Document>
+	</kml>"
+	
+	$kml | Out-File -Force -Encoding ascii ("./BDD/OBSERVATION/$espece.kml")
+	
+}
+
+# INATURALIST
+$files = Get-ChildItem "./BDD/INATURALIST/" -Filter *.csv
+foreach ($f in $files){
+	$fichier = $f.Name
+    $espece = $f.Name -replace ".csv"
+	$kml =
+	"<?xml version=`"1.0`" encoding=`"UTF-8`"?><kml xmlns=`"http://www.opengis.net/kml/2.2`" xmlns:gx=`"http://www.google.com/kml/ext/2.2`" xmlns:kml=`"http://www.opengis.net/kml/2.2`" xmlns:atom=`"http://www.w3.org/2005/Atom`">
+	<Document>
+	<name>$espece</name>
+	<Folder>
+	<name>INATURALIST</name>
+	$(Import-Csv "./BDD/INATURALIST/$fichier" | foreach {'<Placemark><IconStyle><color>red</color><Icon></Icon></IconStyle><Point><coordinates>{1},{0}</coordinates></Point></Placemark>' -f $_.Latitude, $_.Longitude})
+	</Folder>
+	</Document>
+	</kml>"
+	
+	$kml | Out-File -Force -Encoding ascii ("./BDD/INATURALIST/$espece.kml")
+	
+}
 # git
 git config --local user.email "cigalesdefrance@outlook.fr"
 git config --local user.name "cigalesdefrance"
 git add .
-git commit -m "[Bot] Téléchargement des données"
+git commit -m "[Bot] Téléchargement des données + kml"
 git push -f
