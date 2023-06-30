de54413108a27ab3c40005931491e5962ca85678
Adrien CHARBONNEAU <105881448+Adri-Charbonneau@users.noreply.github.com>
2023-06-30 14:30:50 +0200

    Amélioration de l'indentation

--- SCRIPTS/BDD.ps1
+++ SCRIPTS/BDD.ps1
@@ -1,7 +1,9 @@
 $cigales_codes = Import-Csv "CIGALES-CODES.csv"
+
 Remove-item "./BDD/INPN/*.csv"
 Remove-item "./BDD/INATURALIST/*.csv"
 Remove-Item "./BDD/GBIF/*.csv"
+
 $cigales_codes | ForEach-Object {
 	$nom = $_.NOM_SCIENTIFIQUE
 	$onem = $_.ONEM
@@ -11,54 +13,51 @@ $cigales_codes | ForEach-Object {
 	$observation = $_.OBSERVATION
 	$gbif = $_.GBIF
 	
-	#ONEM - voir https://github.com/cigalesdefrance/cigalesdefrance.github.io/blob/main/SCRIPTS/sauvegarde_onem.ps1
+	# ONEM - voir https://github.com/cigalesdefrance/cigalesdefrance.github.io/blob/main/SCRIPTS/sauvegarde_onem.ps1
 	
-	#FAUNE-FRANCE
+	# FAUNE-FRANCE
 	"Faune-France - $nom"
-	if ($faune_france -eq "") {
-	"  > L'espèce n'existe pas dans Faune-France" }
+	if ($faune_france -eq "") { "  > L'espèce n'existe pas dans Faune-France" }
 	else {
-	Invoke-WebRequest -Uri "https://www.faune-france.org/index.php?m_id=95&sp_tg=19&sp_DChoice=all&sp_SChoice=species&sp_PChoice=all&sp_FChoice=map&sp_S=$faune_france" -OutFile "./BDD/FAUNE-FRANCE/$nom.png" }
+		Invoke-WebRequest -Uri "https://www.faune-france.org/index.php?m_id=95&sp_tg=19&sp_DChoice=all&sp_SChoice=species&sp_PChoice=all&sp_FChoice=map&sp_S=$faune_france" -OutFile "./BDD/FAUNE-FRANCE/$nom.png" 
+	}
 	
-	#INPN
+	# INPN
 	"INPN - $nom"
-			if ($inpn -eq "") {
-			"  > L'espèce n'existe pas dans INPN" }
-			else {
-				
-				$totalRecords = (Invoke-WebRequest "https://openobs.mnhn.fr/biocache-service/occurrences/search?fq=taxonConceptID:$inpn" | ConvertFrom-Json).totalRecords
-				if ($totalRecords -eq 0) {
-				"  > L'espèce est présente dans INPN mais ne possède aucune donnée" }
-				else {
-					Add-Content "./BDD/INPN/$nom-coord.csv" "Latitude,Longitude"
-					Add-Content "./BDD/INPN/$nom-id.csv" "ID"
-					$pages = [math]::floor($totalRecords/300)
-					for ($num=0;$num -le $pages;$num++) {
-						if ($num -eq 0) {$startIndex=0} else {$startIndex = ($num*300)}
-						#$startIndex
-						"page $num sur $pages"
-						$json = (Invoke-WebRequest "https://openobs.mnhn.fr/biocache-service/occurrences/search?fq=taxonConceptID:$inpn&startIndex=$startIndex&pageSize=300" | ConvertFrom-Json)
-						$json_filter = $json | Where {$_.occurrences.latLong -ne $null}
-						$latLong = $json_filter.occurrences.latLong | Add-Content "./BDD/INPN/$nom-coord.csv"
-						$id = $json_filter.occurrences.uuid | Add-Content "./BDD/INPN/$nom-id.csv" 
-					}
-					
-					$coord = Get-content "./BDD/INPN/$nom-coord.csv" 
-					$id = Get-content "./BDD/INPN/$nom-id.csv"
-					$(for($index=0;$index -lt $coord.Count;$index++){$coord[$index] + "," + $id[$index]}) | Add-Content "./BDD/INPN/$nom.csv"
-					(Get-Content "./BDD/INPN/$nom.csv") | ? {$_.trim() -ne "" } | Set-Content "./BDD/INPN/$nom.csv"
-					Remove-item "./BDD/INPN/$nom-id.csv"
-					Remove-item "./BDD/INPN/$nom-coord.csv"
-				}}
+	if ($inpn -eq "") { "  > L'espèce n'existe pas dans INPN" }
+	else {
+		$totalRecords = (Invoke-WebRequest "https://openobs.mnhn.fr/biocache-service/occurrences/search?fq=taxonConceptID:$inpn" | ConvertFrom-Json).totalRecords
+		if ($totalRecords -eq 0) {
+		"  > L'espèce est présente dans INPN mais ne possède aucune donnée" }
+		else {
+			Add-Content "./BDD/INPN/$nom-coord.csv" "Latitude,Longitude"
+			Add-Content "./BDD/INPN/$nom-id.csv" "ID"
+			$pages = [math]::floor($totalRecords/300)
+			for ($num=0;$num -le $pages;$num++) {
+				if ($num -eq 0) {$startIndex=0} else {$startIndex = ($num*300)}
+				#$startIndex
+				"page $num sur $pages"
+				$json = (Invoke-WebRequest "https://openobs.mnhn.fr/biocache-service/occurrences/search?fq=taxonConceptID:$inpn&startIndex=$startIndex&pageSize=300" | ConvertFrom-Json)
+				$json_filter = $json | Where {$_.occurrences.latLong -ne $null}
+				$latLong = $json_filter.occurrences.latLong | Add-Content "./BDD/INPN/$nom-coord.csv"
+				$id = $json_filter.occurrences.uuid | Add-Content "./BDD/INPN/$nom-id.csv" 
+			}
+			
+			$coord = Get-content "./BDD/INPN/$nom-coord.csv" 
+			$id = Get-content "./BDD/INPN/$nom-id.csv"
+			$(for($index=0;$index -lt $coord.Count;$index++){$coord[$index] + "," + $id[$index]}) | Add-Content "./BDD/INPN/$nom.csv"
+			(Get-Content "./BDD/INPN/$nom.csv") | ? {$_.trim() -ne "" } | Set-Content "./BDD/INPN/$nom.csv"
+			Remove-item "./BDD/INPN/$nom-id.csv"
+			Remove-item "./BDD/INPN/$nom-coord.csv"
+		}
+	}
 	
-	#INATURALIST
+	# INATURALIST
 	"Inaturalist - $nom"
-	if ($inaturalist -eq "") {
-	"  > L'espèce n'existe pas dans Inaturalist" }
+	if ($inaturalist -eq "") { "  > L'espèce n'existe pas dans Inaturalist" }
 	else {
 		$total_results = (Invoke-WebRequest "https://api.inaturalist.org/v1/observations?&place_id=6753&taxon_id=$inaturalist" | ConvertFrom-Json).total_results
-		if ($total_results -eq 0) {
-		"  > L'espèce est présente dans Inaturalist mais ne possède aucune donnée" }
+		if ($total_results -eq 0) { "  > L'espèce est présente dans Inaturalist mais ne possède aucune donnée" }
 		else {
 			Add-Content "./BDD/INATURALIST/$nom-coord.csv" "Latitude,Longitude"
 			Add-Content "./BDD/INATURALIST/$nom-id.csv" "ID"
@@ -76,63 +75,61 @@ $cigales_codes | ForEach-Object {
 			(Get-Content "./BDD/INATURALIST/$nom.csv") | ? {$_.trim() -ne "" } | Set-Content "./BDD/INATURALIST/$nom.csv"
 			Remove-item "./BDD/INATURALIST/$nom-id.csv"
 			Remove-item "./BDD/INATURALIST/$nom-coord.csv"
-			
-		}}
-		
-		#OBSERVATION.ORG
-		"Observation.org - $nom"
-		if ($observation -eq "") {
-		"  > L'espèce n'existe pas dans Observation" }
+		}
+	}
+	
+	# OBSERVATION.ORG
+	"Observation.org - $nom"
+	if ($observation -eq "") { "  > L'espèce n'existe pas dans Observation" }
+	else {
+		[xml]$data = (invoke-webrequest -Uri "https://france.observation.org/kmlloc/soort_get_xml_points.php?soort=$observation")
+		$observation = $data.markers.line.point
+		if ($observation.count -eq 0) { "  > L'espèce est présente dans Observation mais ne possède aucune donnée" }
 		else {
-			[xml]$data = (invoke-webrequest -Uri "https://france.observation.org/kmlloc/soort_get_xml_points.php?soort=$observation")
-			$observation = $data.markers.line.point
-			if ($observation.count -eq 0) {
-			"  > L'espèce est présente dans Observation mais ne possède aucune donnée" }
-			else {
-				$observation | Out-File "observation.txt"
-				(Get-Content "observation.txt" | Select-Object -Skip 2) | Set-Content "observation.txt"
-				$observation = Get-Content "observation.txt"
-				$observation[0] = "Latitude,Longitude"
-				$observation = $observation -replace " ",","
-				$observation | Out-File "./BDD/OBSERVATION/$nom.csv"
-			Remove-Item "observation.txt" }}
+			$observation | Out-File "observation.txt"
+			(Get-Content "observation.txt" | Select-Object -Skip 2) | Set-Content "observation.txt"
+			$observation = Get-Content "observation.txt"
+			$observation[0] = "Latitude,Longitude"
+			$observation = $observation -replace " ",","
+			$observation | Out-File "./BDD/OBSERVATION/$nom.csv"
+			Remove-Item "observation.txt" 
+		}
+	}
+	
+	# GBIF
+	"GBIF - $nom"
+	if ($GBIF -eq "") { "  > L'espèce n'existe pas dans GBIF" }
+	else {
+		$count = (Invoke-WebRequest "https://api.gbif.org/v1/occurrence/search?country=FR&taxon_key=$gbif" | ConvertFrom-Json).count
+		if ($count -eq 0) { "  > L'espèce est présente dans GBIF mais ne possède aucune donnée" }
+		else {
+			Add-Content "./BDD/GBIF/$nom-coord.csv" "Latitude,Longitude"
+			Add-Content "./BDD/GBIF/$nom-id.csv" "ID"
+			$pages = [math]::floor($count/300)
+			for ($num=0;$num -le $pages;$num++) {
+				if ($num -eq 0) {$offset=0} else {$offset = ($num*300)}
+				#$offset
+				"page $num sur $pages"
+				$lat = (Invoke-WebRequest "https://api.gbif.org/v1/occurrence/search?country=FR&taxon_key=$gbif&offset=$offset&limit=300" | ConvertFrom-Json).results.decimalLatitude | Add-Content "./BDD/GBIF/$nom-lat.csv" 
+				$long = (Invoke-WebRequest "https://api.gbif.org/v1/occurrence/search?country=FR&taxon_key=$gbif&offset=$offset&limit=300" | ConvertFrom-Json).results.decimalLongitude | Add-Content "./BDD/GBIF/$nom-long.csv" 
+				$id = (Invoke-WebRequest "https://api.gbif.org/v1/occurrence/search?country=FR&taxon_key=$gbif&offset=$offset&limit=300" | ConvertFrom-Json).results.key | Add-Content "./BDD/GBIF/$nom-id.csv" 
+			}
 			
-			#GBIF
-			"GBIF - $nom"
-			if ($GBIF -eq "") {
-			"  > L'espèce n'existe pas dans GBIF" }
-			else {
-				
-				$count = (Invoke-WebRequest "https://api.gbif.org/v1/occurrence/search?country=FR&taxon_key=$gbif" | ConvertFrom-Json).count
-				if ($count -eq 0) {
-				"  > L'espèce est présente dans GBIF mais ne possède aucune donnée" }
-				else {
-					Add-Content "./BDD/GBIF/$nom-coord.csv" "Latitude,Longitude"
-					Add-Content "./BDD/GBIF/$nom-id.csv" "ID"
-					$pages = [math]::floor($count/300)
-					for ($num=0;$num -le $pages;$num++) {
-						if ($num -eq 0) {$offset=0} else {$offset = ($num*300)}
-						#$offset
-						"page $num sur $pages"
-						$lat = (Invoke-WebRequest "https://api.gbif.org/v1/occurrence/search?country=FR&taxon_key=$gbif&offset=$offset&limit=300" | ConvertFrom-Json).results.decimalLatitude | Add-Content "./BDD/GBIF/$nom-lat.csv" 
-						$long = (Invoke-WebRequest "https://api.gbif.org/v1/occurrence/search?country=FR&taxon_key=$gbif&offset=$offset&limit=300" | ConvertFrom-Json).results.decimalLongitude | Add-Content "./BDD/GBIF/$nom-long.csv" 
-						$id = (Invoke-WebRequest "https://api.gbif.org/v1/occurrence/search?country=FR&taxon_key=$gbif&offset=$offset&limit=300" | ConvertFrom-Json).results.key | Add-Content "./BDD/GBIF/$nom-id.csv" 
-					}
-					
-					$lat = Get-content "./BDD/GBIF/$nom-lat.csv" 
-					$long = Get-content "./BDD/GBIF/$nom-long.csv" 
-					$(for($index=0;$index -lt $lat.Count;$index++){$lat[$index] + "," + $long[$index]}) | Add-Content "./BDD/GBIF/$nom-coord.csv"
-					(Get-Content "./BDD/GBIF/$nom-coord.csv") | ? {$_.trim() -ne "" } | Set-Content "./BDD/GBIF/$nom-coord.csv"
-					Remove-item "./BDD/GBIF/$nom-lat.csv" 
-					Remove-item "./BDD/GBIF/$nom-long.csv" 
-					
-					$coord = Get-content "./BDD/GBIF/$nom-coord.csv" 
-					$id = Get-content "./BDD/GBIF/$nom-id.csv"
-					$(for($index=0;$index -lt $coord.Count;$index++){$coord[$index] + "," + $id[$index]}) | Add-Content "./BDD/GBIF/$nom.csv"
-					(Get-Content "./BDD/GBIF/$nom.csv") | ? {$_.trim() -ne "" } | Set-Content "./BDD/GBIF/$nom.csv"
-					Remove-item "./BDD/GBIF/$nom-id.csv"
-					Remove-item "./BDD/GBIF/$nom-coord.csv"
-				}}				
+			$lat = Get-content "./BDD/GBIF/$nom-lat.csv" 
+			$long = Get-content "./BDD/GBIF/$nom-long.csv" 
+			$(for($index=0;$index -lt $lat.Count;$index++){$lat[$index] + "," + $long[$index]}) | Add-Content "./BDD/GBIF/$nom-coord.csv"
+			(Get-Content "./BDD/GBIF/$nom-coord.csv") | ? {$_.trim() -ne "" } | Set-Content "./BDD/GBIF/$nom-coord.csv"
+			Remove-item "./BDD/GBIF/$nom-lat.csv" 
+			Remove-item "./BDD/GBIF/$nom-long.csv" 
+			
+			$coord = Get-content "./BDD/GBIF/$nom-coord.csv" 
+			$id = Get-content "./BDD/GBIF/$nom-id.csv"
+			$(for($index=0;$index -lt $coord.Count;$index++){$coord[$index] + "," + $id[$index]}) | Add-Content "./BDD/GBIF/$nom.csv"
+			(Get-Content "./BDD/GBIF/$nom.csv") | ? {$_.trim() -ne "" } | Set-Content "./BDD/GBIF/$nom.csv"
+			Remove-item "./BDD/GBIF/$nom-id.csv"
+			Remove-item "./BDD/GBIF/$nom-coord.csv"
+		}
+	}				
 }
 
 ### CREATION DES KML
@@ -154,14 +151,13 @@ foreach ($f in $files){
 	</kml>"
 	
 	$kml | Out-File -Force -Encoding ascii ("./BDD/INPN/$espece.kml")
-	
 }
 
 # OBSERVATION
 $files = Get-ChildItem "./BDD/OBSERVATION/" -Filter *.csv
 foreach ($f in $files){
 	$fichier = $f.Name
-    $espece = $f.Name -replace ".csv"
+	$espece = $f.Name -replace ".csv"
 	$kml =
 	"<?xml version=`"1.0`" encoding=`"UTF-8`"?><kml xmlns=`"http://www.opengis.net/kml/2.2`">
 	<Document>
@@ -175,14 +171,13 @@ foreach ($f in $files){
 	</kml>"
 	
 	$kml | Out-File -Force -Encoding ascii ("./BDD/OBSERVATION/$espece.kml")
-	
 }
 
 # INATURALIST
 $files = Get-ChildItem "./BDD/INATURALIST/" -Filter *.csv
 foreach ($f in $files){
 	$fichier = $f.Name
-    $espece = $f.Name -replace ".csv"
+	$espece = $f.Name -replace ".csv"
 	$kml =
 	"<?xml version=`"1.0`" encoding=`"UTF-8`"?><kml xmlns=`"http://www.opengis.net/kml/2.2`">
 	<Document>
@@ -197,6 +192,7 @@ foreach ($f in $files){
 	
 	$kml | Out-File -Force -Encoding ascii ("./BDD/INATURALIST/$espece.kml")
 }
+
 # GBIF
 $files = Get-ChildItem "./BDD/GBIF/" -Filter *.csv
 foreach ($f in $files){
@@ -215,9 +211,9 @@ foreach ($f in $files){
 	</kml>"
 	
 	$kml | Out-File -Force -Encoding ascii ("./BDD/GBIF/$espece.kml")
-	
 }
-# git
+
+### SAUVEGARDE GIT
 git config --local user.email "cigalesdefrance@outlook.fr"
 git config --local user.name "CigalesdeFrance-dev"
 git add .
