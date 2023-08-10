ae371605265f512d0c2c9e7d09ef845a826f474e
Adrien CHARBONNEAU <105881448+Adri-Charbonneau@users.noreply.github.com>
2023-08-10 09:26:13 +0200

    Simplification du workflow

---
+++ SCRIPTS/GIT.ps1
@@ -0,0 +1,6 @@
+### SAUVEGARDE GIT
+git config --local user.email "cigalesdefrance@outlook.fr"
+git config --local user.name "CigalesdeFrance-dev"
+git add .
+git commit -m "[Bot] Création des cartes"
+git push -f
