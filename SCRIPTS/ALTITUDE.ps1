986bff89d13b6bea182d3c12cad2a6fd70f9790a
Adrien CHARBONNEAU <105881448+Adri-Charbonneau@users.noreply.github.com>
2025-05-10 18:20:50 +0200

    Conservation des CSV vides

--- SCRIPTS/ALTITUDE.ps1
+++ SCRIPTS/ALTITUDE.ps1
@@ -19,48 +19,48 @@ $bdd_codes | ForEach-Object {
 		# Charger les données
 		$csv = Import-Csv -Path "./BDD/$bdd_nom/$fichier"
 		
-		# Filtrer les lignes avec altitude manquante
-		$alt_to_maj = $csv | Where-Object { -not $_.ALTITUDE -or $_.ALTITUDE -eq "" }
+		if ($csv.Count -eq 0) {
 		
-		# si un seul élément à mettre à jour
-		if ($alt_to_maj -isnot [System.Collections.IEnumerable]) { $alt_to_maj = @($alt_to_maj) }
-		
-		# Diviser en groupes de 100 éléments maximum
-		$group_size = 100
-		$groups = @()
-		for ($i = 0; $i -lt $alt_to_maj.Count; $i += $group_size) {
-			$endIndex = [Math]::Min($i + $group_size - 1, $alt_to_maj.Count - 1)
-			$group_alt = $alt_to_maj[$i..$endIndex]
-			$groups += ,$group_alt
-		}
-		
-		foreach ($group in $groups) {
-
-			$latitudes = ($group | ForEach-Object { $_.LATITUDE }) -join "|"
-			$longitudes = ($group | ForEach-Object { $_.LONGITUDE }) -join "|"
+			# CSV vide donc on ignore
 			
-			$url = "https://data.geopf.fr/altimetrie/1.0/calcul/alti/rest/elevation.json?lon=$longitudes&lat=$latitudes&resource=ign_rge_alti_wld"
+		} else {
 			
-			$response = Invoke-RestMethod -Uri $url -Method Get
+			# Filtrer les lignes avec altitude manquante
+			$alt_to_maj = $csv | Where-Object { -not $_.ALTITUDE -or $_.ALTITUDE -eq "" }
 			
-			# Pause pour respecter la limite de 5 requêtes/sec (https://geoservices.ign.fr/documentation/services/services-geoplateforme/altimetrie)
-			Start-Sleep -Milliseconds 250
+			# si un seul élément à mettre à jour
+			if ($alt_to_maj -isnot [System.Collections.IEnumerable]) { $alt_to_maj = @($alt_to_maj) }
 			
-			# Mettre à jour les altitudes dans le group
-			for ($i = 0; $i -lt $response.elevations.Count; $i++) {
-				$group[$i].ALTITUDE = $response.elevations[$i].z
+			# Diviser en groupes de 100 éléments maximum
+			$group_size = 100
+			$groups = @()
+			for ($i = 0; $i -lt $alt_to_maj.Count; $i += $group_size) {
+				$endIndex = [Math]::Min($i + $group_size - 1, $alt_to_maj.Count - 1)
+				$group_alt = $alt_to_maj[$i..$endIndex]
+				$groups += ,$group_alt
 			}
+			
+			foreach ($group in $groups) {
+				
+				$latitudes = ($group | ForEach-Object { $_.LATITUDE }) -join "|"
+				$longitudes = ($group | ForEach-Object { $_.LONGITUDE }) -join "|"
+				
+				$url = "https://data.geopf.fr/altimetrie/1.0/calcul/alti/rest/elevation.json?lon=$longitudes&lat=$latitudes&resource=ign_rge_alti_wld"
+				
+				$response = Invoke-RestMethod -Uri $url -Method Get
+				
+				# Pause pour respecter la limite de 5 requêtes/sec (https://geoservices.ign.fr/documentation/services/services-geoplateforme/altimetrie)
+				Start-Sleep -Milliseconds 250
+				
+				# Mettre à jour les altitudes dans le group
+				for ($i = 0; $i -lt $response.elevations.Count; $i++) {
+					$group[$i].ALTITUDE = $response.elevations[$i].z
+				}
+			}
+			
+			# Sauvegarder dans le fichier d'origine
+			$csv | Export-Csv -Path "./BDD/$bdd_nom/$fichier" -NoTypeInformation
+			
 		}
-		
-		# Sauvegarder dans le fichier d'origine
-		$csv | Export-Csv -Path "./BDD/$bdd_nom/$fichier" -NoTypeInformation
-		
 	}
 }
-
-### SAUVEGARDE GIT
-git config user.name 'github-actions[bot]'
-git config user.email 'github-actions[bot]@users.noreply.github.com'
-git add .
-git commit -m "[Bot] Calcul de l'altitude"
-git push origin main -f
